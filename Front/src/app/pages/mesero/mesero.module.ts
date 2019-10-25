09b6e47487a21ca62fcb46eb3e683932ba18d09c
jdonlucasp <jdonlucas93@gmail.com>
2019-10-24 23:56:43 -0500

    se crea vista de comanda

--- Front/src/app/pages/mesero/mesero.module.ts
+++ Front/src/app/pages/mesero/mesero.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { MeseroRoutingModule } from './mesero-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ComandaComponent } from './comanda/comanda.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ComandaComponent],
   imports: [
     CommonModule,
     MeseroRoutingModule
