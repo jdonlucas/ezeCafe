5314df41168e4b7a4690985a44deeb4687ed0820
jdonlucasp <jdonlucas93@gmail.com>
2019-12-13 13:19:55 -0600

    se estila y se sube lita de alimentos

--- Front/src/app/pages/mesero/comanda/menu/menu.component.ts
+++ Front/src/app/pages/mesero/comanda/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,13 +11,26 @@ export class MenuComponent implements OnInit {
 
   public orderData: any;
   public orderForm: FormGroup;
+  public foodList: any;
+  public errors: any
 
-  constructor() { }
+  constructor(
+    private _menuService: MenuService,
+    ) { }
 
   ngOnInit() {
     this.orderForm = new FormGroup ({
       name: new FormControl('',[])
     })
+    this.fetchFood();
+  }
+
+  fetchFood() {
+    this._menuService.showFood()
+      .then(response => {
+        this.foodList = response['foodList'];
+      })
+      .catch(err => this.errors = err);
   }
 
 }
