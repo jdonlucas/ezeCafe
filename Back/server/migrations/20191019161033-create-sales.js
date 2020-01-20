0b87c273625cdefd64c8dc1b0a7d2062bc6ee0ac
jdonlucasp <jdonlucas93@gmail.com>
2020-01-20 14:47:01 -0600

    se corrije error comanda duplicada

--- Back/server/migrations/20191019161033-create-sales.js
+++ Back/server/migrations/20191019161033-create-sales.js
@@ -22,7 +22,8 @@ module.exports = {
         references: {
           model: 'Orders',
           key: 'id'
-        }
+        },
+        unique: true
       },
       createdAt: {
         allowNull: false,
