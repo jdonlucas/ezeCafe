bc9a61c2338a1d01ef8cc504d65ff94bb7404ce1
jdonlucasp <jdonlucas93@gmail.com>
2020-02-21 22:14:55 -0600

    se corrigen las horas (again)

--- Back/server/controllers/StatisticsController.js
+++ Back/server/controllers/StatisticsController.js
@@ -99,11 +99,11 @@ var StatisticsController = {
                 for(let i=0;i<salesHistory.length;i++) {
                     if (moment(salesHistory[i].createdAt).format('HH') != startDate) {
                         totalHour.push({ hour: startDate + ':00', total: totalDay })
+                        if( (parseInt(moment(salesHistory[i].createdAt).format('H')) - 1) != parseInt(startDate) ) {
+                            totalHour.push({ hour: (parseInt(moment(salesHistory[i].createdAt).format('H')) - 1) + ':00', total: 0 })
+                        }
                         totalDay = 0.0;
                         startDate = moment(salesHistory[i].createdAt).format('HH');
-                    }
-                    if( (parseInt(moment(salesHistory[i].createdAt).format('H')) - 1) != parseInt(startDate) ) {
-                        totalHour.push({ hour: (parseInt(moment(salesHistory[i].createdAt).format('H')) - 1) + ':00', total: 0 })
                     }
                     totalDay += salesHistory[i].costo;
                 }
