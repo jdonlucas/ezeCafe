c1ae291477c2492432f03c1a2cfb403eef26bb5d
jdonlucasp <jdonlucas93@gmail.com>
2020-04-29 02:37:13 -0500

    dsfsdfs

--- Back/server/migrations/20200429045700-create-extra-order.js
+++ Back/server/migrations/20200429045700-create-extra-order.js
@@ -39,8 +39,7 @@ module.exports = {
         type: Sequelize.DATE
       }
     },
-    { charset: 'latin1',
-      collate: 'latin1_swedish_c1' });
+    { charset: 'latin1'});
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('extraOrders');
