d46b881a6ce9bb7aeb5cad27a9d80eb8356ed2e8
jdonlucasp <jdonlucas93@gmail.com>
2020-04-29 02:18:13 -0500

    dsfsdfs

--- Back/server/config/config.js
+++ Back/server/config/config.js
@@ -2,13 +2,8 @@ require('dotenv').config()
 module.exports = {
   development: {
     use_env_variable:  "DEVELOPMENT_DATABASE_URL",
-    dialect: 'mysql',
-    define: {
-      charset: 'latin1',
-      dialectOptions: {
-        collate: 'latin1_swedish_ci'
-      }
-    }
+    "dialect": "mysql",
+    "define": { "charset": "utf8", "dialectOptions": { "collate": "utf8_general_ci" }
   },
   test: {
     username: "root",
