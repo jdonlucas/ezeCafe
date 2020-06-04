aacb131aefcd427645e5f1f55a36e75a5bc47134
jdonlucasp <jdonlucas93@gmail.com>
2020-06-03 21:32:07 -0500

    se agrega nuevo campo

---
+++ Back/server/migrations/20200501151153-typeOfExtraField.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn('MenuExtras','type', {
+        type: Sequelize.STRING,
+        defaultValue: 'otros'
+      })
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'MenuExtras',
+      'type'
+    );
+  }
+};
